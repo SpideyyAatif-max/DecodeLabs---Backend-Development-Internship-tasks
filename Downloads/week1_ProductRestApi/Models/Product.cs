eec64751d27f83ac12236898228453c8b34218af
SpideyyAatif-max <aatif.fast@gmail.com>
2026-06-16 01:33:45 +0500

    Internship task week1

---
+++ Downloads/week1_ProductRestApi/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProductRestApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public bool InStock { get; set; }
+}
